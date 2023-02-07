a142d481b04e03b2c50e45a54d363ca72caed6c0
Aayam <aayamkhatiwada123@gmail.com>
2023-02-07 19:07:40 +0545

    Changed input field

--- front-end/src/components/RegisterComponent/registerComponent.jsx
+++ front-end/src/components/RegisterComponent/registerComponent.jsx
@@ -2,6 +2,7 @@ import './registerComponent.css';
 import Image from '../assets/resume_builder.jpg';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
 
 const RegisterComponent = () => {
 
@@ -51,31 +52,67 @@ const RegisterComponent = () => {
                         <div className='text-center'>
                             <h2>Register Your account</h2>
                         </div>
-                        <div>
-                            <h5>First Name</h5>
-                            <input type="text" placeholder="Please enter your first name" className="input-box rounded" value={fname} onChange={(e) => setFname(e.target.value)} />
+                        <div className='form-text-field' style={{ marginTop: "3rem" }} >
+                            <TextField
+                                label="First Name"
+                                value={fname}
+                                onChange={(e) => setFname(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <div>
-                            <h5>Last Name</h5>
-                            <input type="text" placeholder="Please enter your last name" className="input-box rounded" value={lname} onChange={(e) => setLname(e.target.value)} />
+                        <div className='form-text-field'>
+                            <TextField
+                                label="Last Name"
+                                value={lname}
+                                onChange={(e) => setLname(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <div>
-                            <h5>Email</h5>
-                            <input type="text" placeholder="Please enter your email address" className="input-box rounded" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <div className='form-text-field'>
+                            <TextField
+                                label="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <div>
-                            <h5>Password</h5>
-                            <input type="password" placeholder="Please enter your password" className="input-box rounded" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <div className='form-text-field'>
+                            <TextField
+                                label="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <div>
-                            <h5>Confirm Password</h5>
-                            <input type="password" placeholder="Please enter your password" className="input-box rounded" value={cpassword} onChange={(e) => setCpassword(e.target.value)} />
+                        <div className='form-text-field'>
+                            <TextField
+                                label="Confirm Password"
+                                value={cpassword}
+                                onChange={(e) => setCpassword(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <div>
-                            <h5>Phone Number</h5>
-                            <input type="text" placeholder="Please enter your phone number" className="input-box rounded" value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)} />
+                        <div className='form-text-field'>
+                            <TextField
+                                label="Phone Number"
+                                value={phoneNo}
+                                onChange={(e) => setPhoneNo(e.target.value)}
+                                fullWidth
+                                InputProps={{ style: { fontSize: 14 } }}
+                                InputLabelProps={{ style: { fontSize: 14 } }}
+                            />
                         </div>
-                        <button type="button" className='btn btn-success button-style' style={{ marginTop: "20px" }} onClick={() => register()}>Register</button>
+                        <button type="button" className='btn btn-success button-style' onClick={() => register()}>Register</button>
                         <button type="button" className='btn btn-light button-style'>Sign Up with Google</button>
                         <p className='text-center'>Already have an account? &nbsp;<a href="/sign-in">Sign In</a></p>
                     </div>
---
+++ front-end/src/routes/createResumePage.jsx
@@ -0,0 +1,8 @@
+const CreateResumePage = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default CreateResumePage;
